69e01b6d1ae7ee978c1995a9701018752b9f12e9
aquacommander <kingblockstar0705@gmail.com>
2026-03-18 10:13:16 +0900

    fix chrome version

--- main.py
+++ main.py
@@ -82,6 +82,11 @@ def main():
     """メインの自動化処理"""
     options = webdriver.ChromeOptions()
     options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 30)
     
